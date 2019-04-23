0f74a26c27ca959011c35e30cff23bae6205722c
Vignesh <vsomnath@student.ethz.ch>
2019-04-23 23:31:09 +0200

    Fixing experiments running. Model running.

--- project1/experiments.py
+++ project1/experiments.py
@@ -1,97 +1,145 @@
-from model import LanguageModel, Dataset
 import numpy as np
 import tensorflow as tf
 import pickle
 import os
-import sys
 import time
 import argparse
+from datetime import datetime
+
+from language_model import LanguageModel
+from dataset import Dataset
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 LOG_DIR = os.path.join(os.path.dirname(__file__), "checkpoints")
+SAVE_DIR = os.path.join(os.path.dirname(__file__), "results")
+PERP_FILE_BASENAME = "group22.perplexity"
 
 def run():
     """Runs the specified experiment."""
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--in_dir", dest="input_dir", default=DATA_DIR, help="input directory")
-    parser.add_argument("--save_dir", dest="save_dir", default=DATA_DIR, help="Save results to")
-    parser.add_argument("--log_dir", dest="logdir", default=LOG_DIR, help="Directory to save checkpoints to.")
-    parser.add_argument("--exp", default="a", help="Which experiment to run")
-    parser.add_argument("--batch_size", type=int, default=64, help="Batch size used.")
-    parser.add_argument("-l", "--max_length", type=int, default=30, help="Maximum length of sentence to use.")
-    parser.add_argument("--lstm_size", type=int, default=512, "Number of hidden units in LSTM.")
-    parser.add_argument("--projection_size", type=int, default=512, help="Projection size")
-    parser.add_argument("--learning_rate", dest="lr", type=float, default=1e-3, help="Learning rate")
+    parser.add_argument("--save_dir", dest="save_dir", default=SAVE_DIR, help="Save results to")
+    parser.add_argument("--log_dir", dest="log_dir", default=LOG_DIR, help="Directory to save checkpoints to.")
+    parser.add_argument("--exp_type", default="a", help="Which experiment to run")
     parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs used.")
-    parser.add_argument("--max_train_time", type=int, default=4 * (3600), help="Max training time in seconds.")
-    parser.add_argument("--embedding_size", type=int, default=100, help="Size of embeddings used.")
+    parser.add_argument("--model_path", default=None, help="Saved model path.")
     args = parser.parse_args()
 
     if not os.path.exists(args.input_dir):
         raise DirectoryNotFoundError("Input directory is missing. Please set it up and download the data.")
 
-    if args.exp_name == "a":
-        experiment_A(args)
-    elif args.exp_name == "b":
-        experiment_B(args)
-    elif args.exp_name == "c":
-        experiment_C(args)
-
-def experiment_A(args):
-    """Runs experiment A."""
+    global dataset
     dataset = Dataset(input_dir=args.input_dir)
     dataset.generate_vocab(max_sen_len=args.max_length, topk=20000, save=True)
-    dataset.parse_sentences(mode="train", save=True, verbose=True)
-    dataset.parse_sentences(mode="eval", save=True)
-    dataset.parse_sentences(mode="test")
+    dataset.parse_train(max_sen_length=args.max_length, save=True, reload=True)
+    dataset.parse_eval(max_sen_length=args.max_length, save=True, reload=True)
+    dataset.parse_test(max_sen_length=args.max_length, save=True, reload=True)
 
-    # Create session object inside the Language Model tomorrow.
-    model = LanguageModel(dataset=dataset, lstm_hidden_size=args.lstm_size,
-                          embedding_size=args.embedding_size, project=False,
-                          pretrained=True, lr=args.lr)
-    model.fit() # Add option for summary
-    model.predict()
+    perp_savefile = PERP_FILE_BASENAME + args.exp_type.upper()
 
-    # Do the perplexity calculations per sentence
-    perpl_file = open("group04.perplexityA", "w")
-    for i in range(len(test_batches)):
-        perplexities = sess.run(model.perplexity,
-                                feed_dict={model.sentence_placeholder: test_batches[i]})
-        if i + 1 == len(test_batches):  # last batch case, reduce result
-            to_take = test_data.shape[0] % batch_size
-            perplexities = perplexities[:to_take]
-        # write to file
-        for p in perplexities:
-            perpl_file.write(str(p) + "\n")
-    perpl_file.close()
+    if args.exp_type == "a":
+        model = experiment_A(args)
+    elif args.exp_type == "b":
+        model = experiment_B(args)
+    elif args.exp_type == "c":
+        model = experiment_C(args)
+    elif args.exp_type == "d":
+        experiment_D(args)
+        return
+    else:
+        raise ValueError("Experiment of type {} not found.".format(args.exp_type))
+
+    if args.model_path is None:
+        print("Training model...")
+        model.fit(num_epochs=args.num_epochs, batch_size=64, verbose=True)
+    else:
+        print("Computing test perplexities.")
+        model.save_perplexity_to_file(filename=perp_savefile)
+
+def experiment_A(args):
+    exp_name = "Experiment_A_" + datetime.now().strftime("%H:%M:%S")
+    exp_logdir = os.path.join(args.log_dir, exp_name)
+    exp_savedir = os.path.join(args.save_dir, exp_name)
+
+    restore_from = None
+    if args.model_path is not None:
+        restore_from = os.path.join(args.save_dir, args.model_path)
+
+    if not os.path.exists(exp_logdir):
+        os.makedirs(exp_logdir)
+
+    model = LanguageModel(dataset=dataset, lstm_hidden_size=512,
+                          embedding_size=100, project=False,
+                          pretrained=False, save_dir=exp_savedir,
+                          log_dir=exp_logdir,
+                          restore_from=restore_from)
+    return model
 
 def experiment_B(args):
-    """Runs experiment B."""
-    dataset = Dataset(input_dir=args.input_dir)
+    exp_name = "Experiment_B_" + datetime.now().strftime("%H:%M:%S")
+    exp_logdir = os.path.join(args.log_dir, exp_name)
+    exp_savedir = os.path.join(args.save_dir, exp_name)
 
-    with tf.Session() as sess:
-        model = LanguageModel(num_hidden_units=args.lstm_size, time_steps=29,
-                              len_corpus=len(words), batch_size=args.batch_size,
-                              vocab=words, session=sess, lr=args.lr,
-                              embed_path="..", #TODO:
-                              embedding_length=args.embedding_length)
-        model.train() # Add option for summary
-        model.predict()
+    restore_from = None
+    if args.model_path is not None:
+        restore_from = os.path.join(args.save_dir, args.model_path)
+
+    if not os.path.exists(exp_logdir):
+        os.makedirs(exp_logdir)
+
+    model = LanguageModel(dataset=dataset,
+                          lstm_hidden_size=512,
+                          embedding_size=100,
+                          project=False,
+                          pretrained=True,
+                          save_dir=exp_savedir,
+                          log_dir=exp_logdir,
+                          restore_from=restore_from)
+    return model
 
 def experiment_C(args):
-    """Runs experiment C."""
-    dataset = Dataset(input_dir=args.input_dir)
-    with tf.Session() as sess:
-        model = LanguageModel(num_hidden_units=args.lstm_size, time_steps=29,
-                              len_corpus=len(words), batch_size=args.batch_size,
-                              add_projection=True, projection_size=args.projection_size,
-                              vocab=words, session=sess, lr=args.lr,
-                              embedding_length=args.embedding_length)
-        model.train() # Add option for summary
-        model.predict()
+    exp_name = "Experiment_C_" + datetime.now().strftime("%H:%M:%S")
+    exp_logdir = os.path.join(args.log_dir, exp_name)
+    exp_savedir = os.path.join(args.save_dir, exp_name)
+
+    restore_from = None
+    if args.model_path is not None:
+        restore_from = os.path.join(args.save_dir, args.model_path)
+
+    if not os.path.exists(exp_logdir):
+        os.makedirs(exp_logdir)
+
+    model = LanguageModel(dataset=dataset,
+                          lstm_hidden_size=1024,
+                          embedding_size=100,
+                          project=True,
+                          project_size=512,
+                          pretrained=True,
+                          log_dir=exp_logdir,
+                          save_dir=exp_savedir,
+                          restore_from=restore_from)
+    return model
+
+def experiment_D(args):
+    exp_name = "Experiment_C_" + datetime.now().strftime("%H:%M:%S")
+    exp_logdir = os.path.join(args.log_dir, exp_name)
+    exp_savedir = os.path.join(args.save_dir, exp_name)
+    restore_from = os.path.join(args.save_dir, args.model_path)
+
+    model = LanguageModel(dataset=dataset,
+                          lstm_hidden_size=1024,
+                          embedding_size=100,
+                          project=True,
+                          project_size=512,
+                          pretrained=True,
+                          log_dir=exp_logdir,
+                          save_dir=exp_savedir,
+                          restore_from=restore_from)
+
+    model.complete_sentences(dataset.continuation, max_len=20)
 
 if __name__ == "__main__":
     run()
