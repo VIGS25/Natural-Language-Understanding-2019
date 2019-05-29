471121e657fb677442d7962d61e833fde4763cef
Vignesh <vsomnath@student.ethz.ch>
2019-05-29 12:03:45 +0200

    Download script

---
+++ project2/data/download_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download -O stories.train.csv
+wget https://polybox.ethz.ch/index.php/s/02IVLdBAgVcsJAx/download -O stories.eval.csv
+#wget https://polybox.ethz.ch/index.php/s/6RELTQ3CapEBSKE/download -O stories.test.csv
+#wget https://polybox.ethz.ch/index.php/s/h2gp3FpS3N7Xgiq/download -O stories.spring2016.csv
+
+wget "http://download.tensorflow.org/models/skip_thoughts_uni_2017_02_02.tar.gz"
+tar -xvf skip_thoughts_uni_2017_02_02.tar.gz
+rm -rf skip_thoughts_uni_2017_02_02.tar.gz uni
+mv skip_thoughts_uni_2017_02_02 uni
+
+wget "http://download.tensorflow.org/models/skip_thoughts_bi_2017_02_16.tar.gz"
+tar -xvf skip_thoughts_bi_2017_02_16.tar.gz
+rm -rf skip_thoughts_bi_2017_02_16.tar.gz bi
+mv skip_thoughts_bi_2017_02_16 bi
