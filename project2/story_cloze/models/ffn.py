5573c14aa1028a00f7474edd8010edcf9be64a0b
Vignesh <vsomnath@student.ethz.ch>
2019-05-30 22:06:11 +0200

    Running FFN model.

--- project2/story_cloze/models/ffn.py
+++ project2/story_cloze/models/ffn.py
@@ -6,29 +6,50 @@ import numpy as np
 import tensorflow as tf
 import os
 from typing import List, Dict, Tuple
+import logging
 
 from .base_model import Model
 
 DEF_MODEL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../outputs"))
 
+def get_rnn_cell(rnn_type="gru", num_hidden_units=1000):
+    """Returns initialized RNN cells based on type."""
+    if rnn_type == "gru":
+        return tf.nn.rnn_cell.GRUCell(num_units=num_hidden_units)
+    elif rnn_type == "lstm":
+        return tf.nn.rnn_cell.LSTMCell(num_units=num_hidden_units)
+    elif rnn_type == "vanilla":
+        return tf.nn.rnn_cell.BasicRNNCell(num_units=num_hidden_units)
+    else:
+        raise ValueError("RNN type {} not supported.".format(RNN))
+
+logger = logging.getLogger(__name__)
 
 class FFN(Model):
 
     model_dir = DEF_MODEL_DIR
 
+    train_states = list()
+    eval_states = list()
+
+    train_outputs = list()
+    eval_outputs = list()
+
     def __init__(self,
                  embedding_dim: int = 4800,
+                 rnn_type: str = "gru",
                  input_mode: str = "full_context",
-                 num_hidden_units: int  = 4800,
                  hidden_layer_sizes: List[int] = [256, 64],
                  n_story_sentences: int = 4,
                  trainable_zero_state: bool = False,
-                 tensorboard_log_frequency: int = 10,
+                 clip_norm: float = 10.0,
+                 max_checkpoints_to_keep: int = 5,
                  learning_rate: float = 0.01,
                  model_dir: str = None,
                  log_dir: str = None,
                  restore_from: str = None,
                  **kwargs):
+
         """
         Parameters
         ----------
@@ -52,68 +73,137 @@ class FFN(Model):
         if input_mode not in ["full_context", "last_sentence", "no_context"]:
             raise ValueError("Input mode {} not recognized.".format(input_mode))
         self.hidden_layer_sizes = hidden_layer_sizes
+        self.rnn_type = rnn_type
         self.input_mode = input_mode
         self.trainable_zero_state = trainable_zero_state
         self.embedding_dim = embedding_dim
         self.n_story_sentences = n_story_sentences
-        self.num_hidden_units = num_hidden_units
-        self.learning_rate = learning_rate
-        if model_dir is not None:
-            self.model_dir = model_dir
-        self.tensorboard_log_frequency = tensorboard_log_frequency
-        self.restore_from = restore_from
+        self.clip_norm = clip_norm
+        self.num_hidden_units = embedding_dim
+
+        super(FFN, self).__init__(learning_rate=learning_rate,
+                                  model_dir=model_dir, log_dir=log_dir,
+                                  max_checkpoints_to_keep=max_checkpoints_to_keep,
+                                  restore_from=restore_from, **kwargs)
 
         self._build_tf_objects()
 
     def _build_placeholders(self):
         if self.input_mode == "full_context":
-            self.input_ph = tf.placeholder(dtype=tf.float32, name='Full_context',
+            self.train_input = tf.placeholder(dtype=tf.float32, name='Full_context',
+                                          shape=[None, self.n_story_sentences, self.embedding_dim])
+            self.eval_input = tf.placeholder(dtype=tf.float32, name='Eval_context',
                                           shape=[None, self.n_story_sentences, self.embedding_dim])
         elif self.input_mode == "last_sentence":
-            self.input_ph = tf.placeholder(dtype=tf.float32, name='Last_sentence', shape=[None, self.embedding_dim])
+            self.train_input = tf.placeholder(dtype=tf.float32, name='Last_sentence', shape=[None, 1, self.embedding_dim])
+            self.eval_input = tf.placeholder(dtype=tf.float32, name='Last_eval_sentence', shape=[None, 1, self.embedding_dim])
         else:
-            self.input_ph = tf.zeros(shape=[None, self.embedding_dim])
-        self.ending_ph = tf.placeholder(dtype=tf.float32, name='Ending', shape=[None, self.embedding_dim])
-        self.label_ph = tf.placeholder(dtype=tf.float32, name='Labels', shape=[None, 1])
-        self.batch_size = tf.shape(self.input_ph)[0]
+            self.train_input = tf.zeros(shape=[None, self.embedding_dim])
+            self.eval_input = tf.zeros(shape=[None, self.embedding_dim])
 
-    def _build_rnn(self):
-        """Builds the GRU and performs the unrolling."""
-        self.states = list()
-        self.outputs = list()
+        self.ending_ph = tf.placeholder(dtype=tf.float32, name='Ending', shape=[None, 1, self.embedding_dim])
+        self.labels_ph = tf.placeholder(dtype=tf.int32, name='Labels', shape=[None, 1])
+        self.batch_size = tf.shape(self.train_input)[0]
 
-        with tf.variable_scope("GRU", reuse=tf.AUTO_REUSE):
-            self.rnn_cell = tf.nn.rnn_cell.GRUCell(num_units=self.num_hidden_units)
-            state = self.rnn_cell.zero_state(batch_size=self.batch_size, dtype=tf.float32)
-            self.states.append(state)
+        self.eval_ending1 = tf.placeholder(dtype=tf.float32, name='Eval_Ending1', shape=[None, 1, self.embedding_dim])
+        self.eval_ending2 = tf.placeholder(dtype=tf.float32, name='Eval_Ending2', shape=[None, 1, self.embedding_dim])
+
+        self.eval_accuracy_ph = tf.placeholder(tf.float32)
+        self.eval_act1_ph = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='Eval_act1')
+        self.eval_act2_ph = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="Eval_act2")
+
+    def _unroll_rnn_cell(self, state, mode="train"):
+        if mode == "train":
+            logger.info("Unrolling RNN in train mode.")
 
             for timestep in range(self.n_story_sentences):
-                out, state = self.rnn_cell(self.input_ph[:, timestep], state)
-                self.states.append(state)
-                self.outputs.append(out)
+                out, state = self.rnn_cell(self.train_input[:, timestep], state)
+                self.train_states.append(state)
+                self.train_outputs.append(out)
 
-            self.context_tensor = out
-
-    def _build_model_graph(self):
-        if self.input_mode == "full_context":
-            self._build_rnn()
         else:
-            self.context_tensor = self.input_ph
-        ffn_tensor = tf.add(self.context_tensor, self.ending_ph)
+            logger.info("Unrolling RNN in eval mode.")
+            for time_step in range(self.n_story_sentences):
+                out, state = self.rnn_cell(self.eval_input[:, time_step], state)
+                self.eval_outputs.append(out)
+                self.eval_states.append(state)
 
+        return out
+
+    def _build_rnn(self, mode="train"):
+        """Builds the RNN and performs the unrolling."""
+        with tf.variable_scope(self.rnn_type.upper(), reuse=tf.AUTO_REUSE):
+            self.rnn_cell = get_rnn_cell(rnn_type=self.rnn_type, num_hidden_units=self.num_hidden_units)
+            state = self.rnn_cell.zero_state(batch_size=self.batch_size, dtype=tf.float32)
+            if mode == "train":
+                self.train_states.append(state)
+            else:
+                self.eval_states.append(state)
+            context_tensor = self._unroll_rnn_cell(state=state, mode=mode)
+
+        return context_tensor
+
+    def _build_fc_layers(self, inputs, reuse=tf.AUTO_REUSE):
         with tf.variable_scope("FFN", reuse=tf.AUTO_REUSE):
             for idx, layer_size in enumerate(self.hidden_layer_sizes):
-                ffn_tensor = tf.layers.dense(inputs=ffn_tensor, units=layer_size, activation=tf.nn.relu)
-                print("Layer {} output shape: {} ".format(idx, ffn_tensor.shape))
+                inputs = tf.layers.dense(inputs=inputs, units=layer_size, activation=tf.nn.relu)
+                logger.info("Layer {} output shape: {} ".format(idx, inputs.shape))
 
-            self.logits = tf.layers.dense(ffn_tensor, units=2, activation=tf.nn.relu)
-            ending_probs = tf.nn.softmax(self.logits)
-            self.correct_ending_prob = tf.reduce_max(ending_probs, axis=1, keepdims=True)
+            logits = tf.layers.dense(inputs, units=2, activation=tf.nn.relu)
+        return logits
 
-        labels = tf.one_hot(self.label_ph, depth=2)
-        labels = tf.stop_gradient(labels)
-        with tf.name_scope("Loss"):
-            self.loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=labels)
+    def _build_model_graph(self, mode="train"):
+        if self.input_mode == "full_context":
+            context_tensor = self._build_rnn(mode=mode)
+        else:
+            if mode == "train":
+                context_tensor = self.train_input
+            else:
+                context_tensor = self.eval_input
+
+        if mode == "train":
+            ending_ph = tf.squeeze(self.ending_ph, axis=1)
+            inputs = tf.add(context_tensor, ending_ph)
+            logger.info("Inputs to FFN: {}".format(inputs.shape))
+            self.train_logits = self._build_fc_layers(inputs)
+            self.train_probs = tf.reduce_max(tf.nn.softmax(self.train_logits), axis=1)
+            self.train_predictions = tf.cast(tf.argmax(self.train_logits, axis=-1), tf.int32)
+
+            logger.info("Train Logits: {}".format(self.train_logits.shape))
+            logger.info("Train probs: {}".format(self.train_probs.shape))
+            logger.info("Train Predictions: {}".format(self.train_predictions.shape))
+
+            self.train_accuracy = tf.reduce_mean(tf.to_float(tf.equal(self.labels_ph, self.train_predictions)))
+
+            labels = tf.to_float(tf.one_hot(self.labels_ph, depth=2))
+            labels = tf.stop_gradient(labels)
+            with tf.name_scope("Loss"):
+                self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.train_logits, labels=labels))
+
+        else:
+            eval_ending1 = tf.squeeze(self.eval_ending1, axis=1)
+            eval_ending2 = tf.squeeze(self.eval_ending2, axis=1)
+
+            inputs1 = tf.add(context_tensor, eval_ending1)
+            inputs2 = tf.add(context_tensor, eval_ending2)
+
+            logger.info("Eval input1: {}".format(inputs1.shape))
+            logger.info("Eval input2: {}".format(inputs2.shape))
+
+            self.eval_logits1 = self._build_fc_layers(inputs1)
+            self.eval_logits2 = self._build_fc_layers(inputs2)
+
+            logger.info("Eval logits1: {}".format(self.eval_logits1.shape))
+            logger.info("Eval logits2: {}".format(self.eval_logits2.shape))
+
+            eval_probs1 = tf.nn.softmax(self.eval_logits1)
+            eval_probs2 = tf.nn.softmax(self.eval_logits2)
+
+            self.eval_probs1 = tf.expand_dims(eval_probs1[:, 1], axis=1)
+            self.eval_probs2 = tf.expand_dims(eval_probs2[:, 1], axis=1)
+
+            correct_ending_probs = tf.concat([self.eval_probs1, self.eval_probs2], axis=1)
+            self.eval_predictions = tf.argmax(correct_ending_probs, axis=1)
 
         with tf.name_scope("optimizer"):
             self._build_optimizer()
@@ -129,56 +219,76 @@ class FFN(Model):
         else:
             self.optimizer = optimizer
 
+    def _build_train_summaries(self):
+        self._train_summaries.extend([tf.summary.histogram("train/activations", self.train_probs),
+                                      tf.summary.scalar("train/loss", self.loss),
+                                      tf.summary.scalar("train/accuracy", self.train_accuracy)])
+
+        self.merged_train_summaries = tf.summary.merge(self._train_summaries, name="train_summaries")
+
+    def _build_eval_summaries(self):
+        self._eval_summaries.extend([tf.summary.scalar('eval/accuracy', self.eval_accuracy_ph),
+                                     tf.summary.histogram('eval/activations1', self.eval_act1_ph),
+                                     tf.summary.histogram('eval/activations2', self.eval_act2_ph)])
+        self.merged_eval_summaries = tf.summary.merge(self._eval_summaries, name="eval_summaries")
+
     def _evaluate_batch(self, eval_sentences):
         """Computes metrics on eval batches."""
         eval_story = eval_sentences[:, :self.n_story_sentences]
         eval_ending1 = eval_sentences[:, self.n_story_sentences]
         eval_ending2 = eval_sentences[:, self.n_story_sentences + 1]
 
+        eval_ending1 = np.expand_dims(eval_ending1, axis=1)
+        eval_ending2 = np.expand_dims(eval_ending2, axis=1)
+
+        feed_dict = {}
         if self.input_mode == "full_context":
-            assert list(eval_in1.shape[1:]) == tf.shape(self.input_ph).as_list()[1:]
-            feed_dict_1 = {self.input_ph: eval_story, self.ending_ph: eval_ending1}
-            feed_dict_2 = {self.input_ph: eval_story, self.ending_ph: eval_ending2}
+            feed_dict = {self.eval_input: eval_story, self.train_input: eval_story}
 
         elif self.input_mode == "last_sentence":
-            feed_dict_1 = {self.input_ph: eval_story[:, -1], self.ending_ph: eval_ending1}
-            feed_dict_2 = {self.input_ph: eval_story[:, -1], self.ending_ph: eval_ending2}
+            feed_dict = {self.eval_input: eval_story[:, -1], self.train_input: eval_story[:, -1]}
 
-        else:
-            feed_dict_1 = {self.ending_ph: eval_ending1}
-            feed_dict_2 = {self.ending_ph: eval_ending2}
+        feed_dict[self.eval_ending1] = eval_ending1
+        feed_dict[self.eval_ending2] = eval_ending2
 
-        fetches = self.correct_ending_prob
+        fetches = [self.eval_predictions, self.eval_probs1, self.eval_probs2]
+        results = self._get_tf_object("Session").run(fetches=fetches, feed_dict=feed_dict)
 
-        corr_ending_prob1 = self._get_tf_object("Session").run(feed_dict=feed_dict_1, fetches=fetches)
-        corr_ending_prob2 = self._get_tf_object("Session").run(feed_dict=feed_dict_2, fetches=fetches)
-
-        corr_ending_probs = np.concatenate([corr_ending_prob1, corr_ending_prob2], axis=1)
-        endings_pred = np.argmax(corr_ending_probs)
-
-        return endings_pred
+        return results
 
     def _train_batch(self, train_batch, add_summary=False, verbose=False):
         """Runs the training on every batch."""
-        train_sentences, train_labels = train_batch
-        fetches = [self.loss,  self.train_op, self.logits]
+        encoded_train, train_labels = train_batch
 
-        assert len(train_sentences.shape) == 3
-        assert train_sentences.shape[1:] == tf.shape(self.input_ph).as_list()[1:]
+        train_story = encoded_train[:, :self.n_story_sentences]
+        train_ending = np.expand_dims(encoded_train[:, -1], axis=1)
+
+        fetches = [self.loss,  self.train_op, self.train_predictions, self.train_accuracy]
+        if add_summary:
+            fetches.append(self.merged_train_summaries)
+
+        assert len(encoded_train.shape) == 3
 
         if len(train_labels.shape) == 1:
             train_labels = train_labels.reshape(-1, 1)
+        train_labels = train_labels.astype(np.int32)
 
-        feed_dict = {self.input_ph: train_sentences, self.label_ph: train_labels}
-        loss, _, logits = self._get_tf_object("Session").run(fetches=fetches, feed_dict=feed_dict)
+        feed_dict = {}
+        if self.input_mode == "full_context":
+            feed_dict = {self.train_input: train_story}
 
-        predictions = np.argmax(logits, axis=1)
-        train_accuracy_batch = np.mean(predictions == train_labels)
+        elif self.input_mode == "last_sentence":
+            feed_dict = {self.train_input: train_story[:, -1]}
+
+        feed_dict[self.labels_ph] = train_labels
+        feed_dict[self.ending_ph] = train_ending
+        results = self._get_tf_object("Session").run(fetches=fetches, feed_dict=feed_dict)
 
         if add_summary:
-            pass
+            timestep = self._get_tf_object("Session").run(self._get_tf_object("GlobalStep"))
+            self._get_tf_object("FileWriter").add_summary(results[-1], timestep)
         if verbose:
-            print("Loss: {0:.4f}, Train accuracy: {1:.3f}".format(loss, train_accuracy_batch))
+            logger.info("Loss: {0:.4f}, Train accuracy: {1:.3f}".format(results[0], results[3]))
 
 if __name__ == "__main__":
     ffn = FFN()
---
+++ project2/train_ffn.py
@@ -0,0 +1,100 @@
+"""
+Train script for RNN model.
+"""
+
+import argparse
+from datetime import datetime as dt
+import os
+import numpy as np
+import logging
+
+from story_cloze import Dataset
+from story_cloze.embeddings import SkipThoughts, UniversalEncoder
+from story_cloze.models import FFN
+
+DEFAULT_INPUT_DIR = os.path.join(os.environ["SCRATCH"], "data")
+# DEFAULT_INPUT_DIR = None
+DEFAULT_LOG_DIR = "./logs"
+DEFAULT_MODEL_DIR = "./checkpoints"
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # I/O
+    parser.add_argument("--input_dir", default=DEFAULT_INPUT_DIR, help="Directory where data is present.")
+    parser.add_argument("--log_dir", default=DEFAULT_LOG_DIR, help="Where to save Tensorboard-Logs to")
+    parser.add_argument("--model_dir", default=DEFAULT_MODEL_DIR, help="Where to save models to.")
+    parser.add_argument("--restore_from", default=None, help="Where to restore pretrained model from.")
+    parser.add_argument("--max_checkpoints_to_keep", default=5, type=int, help="How many checkpoints to keep.")
+
+    # Setup specific
+    parser.add_argument("--batch_size", default=64, type=int, help="Batch Size used.")
+    parser.add_argument("--rnn_type", default="gru", help="Type of RNN used.")
+    parser.add_argument("--input_mode", default="full_context", choices=["full_context", "last_sentence", "no_context"], help="Input mode.")
+    parser.add_argument("--encoder_type", default="skipthoughts", choices=["skipthoughts", "universal"], help="Encoder type")
+    parser.add_argument("--embed_mode", default="bi", choices=["bi", "uni", "both"], help="Embeddings to use for SkipThoughts")
+    parser.add_argument("--clip_norm", type=float, default=10.0, help="Gradient clipping norm")
+    parser.add_argument("--trainable_zero_state", action="store_true", help="Whether to train zero state.")
+
+    # Dataset specific
+    parser.add_argument("--story_length", type=int, default=4, help="Size of story used.")
+    parser.add_argument("--n_random", type=int, default=1, help="Number of random endings generated")
+
+    # Training specific
+    parser.add_argument("--learning_rate", default=0.001, type=float, help="Learning rate used.")
+    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs for training.")
+    parser.add_argument("--log_every", type=int, default=100, help="Log stats every.")
+    parser.add_argument("--print_every", type=int, default=100, help="Print stats every.")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    if args.encoder_type == "skipthoughts":
+        embedding_dir = os.path.join(args.input_dir, "embeddings", "skip_thoughts")
+        if args.embed_mode == "uni" or args.embed_mode == "bi":
+            embedding_dim = 2400
+        else:
+            embedding_dim = 4800
+        encoder = SkipThoughts(embed_dir=embedding_dir, mode=args.embed_mode)
+    elif args.encoder_type == "universal":
+        encoder = UniversalEncoder()
+        embedding_dim = 512
+    else:
+        raise ValueError("Encoder of type {} is not supported.".format(args.encoder_type))
+
+    exp_name = "Roemelle_RNN_" + dt.now().strftime("%d-%m-%Y--%H-%M-%S")
+    model_dir = os.path.join(args.model_dir, exp_name)
+    log_dir = os.path.join(args.log_dir, exp_name)
+
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    logger.info("Starting to run the experiment {}".format(exp_name))
+    print("Parameters used: {}".format(args))
+    dataset = Dataset(encoder=encoder,
+                      story_length=args.story_length,
+                      input_dir=args.input_dir,
+                      use_small=True,
+                      n_random=args.n_random)
+
+    logger.info("Building the model...")
+    model = FFN(embedding_dim=embedding_dim,
+                rnn_type=args.rnn_type,
+                learning_rate=args.learning_rate,
+                num_hidden_units=embedding_dim,
+                n_story_sentences=args.story_length,
+                clip_norm=args.clip_norm,
+                model_dir=model_dir, log_dir=log_dir,
+                max_checkpoints_to_keep=args.max_checkpoints_to_keep,
+                trainable_zero_state=args.trainable_zero_state,
+                restore_from=args.restore_from)
+
+    logger.info("Training the model...")
+    model.fit(dataset, nb_epochs=args.num_epochs)
+
+if __name__ == "__main__":
+    main()
