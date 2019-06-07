2fc9f76bbfdcfeb151275864033a0f1a66f8f645
Vignesh <vsomnath@student.ethz.ch>
2019-06-07 11:50:43 +0200

    Added bidirectionality

--- project2/story_cloze/models/__init__.py
+++ project2/story_cloze/models/__init__.py
@@ -1,5 +1,6 @@
 from .ffn import FFN
 from .roemelle_rnn import RNN
+from .roemelle_birnn import BiRNN
 from .base_model import Model
 
-__all__ = ("RNN", "FFN", "Model")
+__all__ = ("RNN", "FFN", "Model", "BiRNN")
