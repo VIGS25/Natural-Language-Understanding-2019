47bc72aaf30cdf520c277a1be66aed1b6991dd24
Vignesh <vsomnath@student.ethz.ch>
2019-06-10 21:53:14 +0200

    Fixes to Setup

---
+++ project2/experiments/roemelle_lstm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+bsub -n 20 -N -W 10:00 -R "rusage[mem=10240, ngpus_excl_p=1]" python -m train_rnn \
+    --batch_size 100 \
+    --rnn_type lstm \
+    --num_hidden_units 1000 \
+    --encoder_type skipthoughts \
+    --embed_mode both \
+    --clip_norm 10 \
+    --story_length 4 \
+    --n_random 6 \
+    --learning_rate 1e-3 \
+    --num_epochs 20 \
+    --log_every 1000 \
+    --print_every 1000 \
+    --eval_every 1000
---
+++ project2/experiments/train_birnn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+module load python_gpu/3.6.4 cuda/10.0.130 cudnn/7.2
+
+bsub -n 20 -N -W 20:00 -R "rusage[mem=10240, ngpus_excl_p=1]" python -m train_birnn \
+    --max_checkpoints_to_keep 10 \
+    --batch_size 100 \
+    --rnn_type lstm \
+    --num_hidden_units 1000 \
+    --encoder_type skipthoughts \
+    --embed_mode both \
+    --clip_norm 10 \
+    --story_length 4 \
+    --n_random 6 \
+    --learning_rate 1e-3 \
+    --num_epochs 20 \
+    --log_every 1000 \
+    --print_every 1000 \
+    --eval_every 1000
+
