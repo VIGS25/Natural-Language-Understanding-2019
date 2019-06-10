f4068b337381297f0c3617e2ee88a7b7c44261cd
Vignesh <vsomnath@student.ethz.ch>
2019-06-11 00:15:41 +0200

    Fixes to experiments running

---
+++ project2/experiments/train_use.sh
@@ -0,0 +1,17 @@
+alias submit="bsub -n 4 -W 24:00 -R \"rusage[mem=18000, ngpus_excl_p=1]\" python"
+mkdir -p logs/
+
+ENCODER="--encoder_type universal"
+
+for RNN_TYPE in "gru" "lstm"
+do
+	submit story_cloze.train.train_rnn.py $ENCODER --rnn_type $RNN_TYPE &
+	submit story_cloze.train.train_ffn.py $ENCODER --rnn_type $RNN_TYPE &
+	submit story_cloze.train.train_birnn.py $ENCODER --rnn_type $RNN_TYPE &
+
+	for ATT_TYPE in "multiplicative" "additive"
+	do
+		submit story_cloze.train.train_rnn.py $ENCODER --rnn_type $RNN_TYPE --use_attn --attn_type $ATT_TYPE &
+		submit story_cloze.train.train_birnn.py $ENCODER --rnn_type $RNN_TYPE --use_attn --attn_type $ATT_TYPE  &
+	done
+done
