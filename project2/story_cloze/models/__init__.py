84cb6ad7dbdd3a52d4b3e2aea56881015d363bf2
Vignesh <vsomnath@student.ethz.ch>
2019-06-11 02:20:07 +0200

    Fix rename

--- project2/story_cloze/models/__init__.py
+++ project2/story_cloze/models/__init__.py
@@ -1,6 +1,6 @@
 from .ffn import FFN
-from .roemelle_rnn import RNN
-from .roemelle_birnn import BiRNN
+from .roemmele_rnn import RNN
+from .roemmele_birnn import BiRNN
 from .base_model import Model
 
 __all__ = ("RNN", "FFN", "Model", "BiRNN")
